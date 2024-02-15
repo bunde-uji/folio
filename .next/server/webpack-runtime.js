a5a1e3b90777a5a58b5992d6ff4d64afca3c6813
bunde-uji <bundeuji@gmail.com>
2024-02-15 19:50:18 +0100

    minor refactoring

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("edd9c3944dbff6e9")
+/******/ 		__webpack_require__.h = () => ("69be8af16523da5c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "14cc19480a6adf78"; }
+/******/ 		__webpack_require__.h = function() { return "aff128af15faf99a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
